63d64bc8e4a2ca5eb42c6bef25158e66ff29ea7a
just4fun <houritsunohikari@gmail.com>
2019-07-15 21:49:45 +0800

    feat: example to call express post api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    funText: null
+  }
+
+  handleButtonClick = () => {
+    fetch('api/have_some_fun', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: 'just4fun'
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        funText: response.data
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Click button below to call Node API.
+          </p>
+          <button
+            onClick={this.handleButtonClick}>
+            Click me!
+          </button>
+          <p className="App-response">
+            {this.state.funText}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+app.post('/api/have_some_fun', (req, res) => {
+  console.log(`request from: *** ${req.body.name} ***`);
+  res.status(200).json({ data: 'Here is your fun'});
+});
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
